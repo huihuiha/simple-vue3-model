c2a5c6b4d6e6661239722b6e0dd4fcd1cfbb9144
linguanghui <guanghui.lin@lixinchuxing.com>
2022-08-09 20:45:12 +0800

    feat: 实现effect scheduler

--- src/reactive/effect.ts
+++ src/reactive/effect.ts
@@ -1,7 +1,25 @@
+// 存放当前 dep 实例
 let effectActive;
-export const effect = (fn: () => any) => {
+// 存放targer -> key -> value 的关系
+const targetMap = new Map();
+
+// 相当于对fn做了一层封装
+class TargetKey {
+    public _fn = () => {};
+
+    constructor(fn, public scheduler?) {
+        this._fn = fn;
+    }
+
+    run() {
+        effectActive = this;
+        return this._fn();
+    }
+}
+
+export const effect = (fn: () => any, option: any = {}) => {
     // 中间套一层对象，目的是用于存储
-    const effectFn = new TargetKey(fn);
+    const effectFn = new TargetKey(fn, option.scheduler);
 
     // 执行effect传入的fn
     effectFn.run();
@@ -9,23 +27,7 @@ export const effect = (fn: () => any) => {
     return effectFn.run.bind(effectFn);
 }
 
-// 相当于对fn做了一层封装
-class TargetKey {
-    public _fn = () => {};
-
-    constructor(fn) {
-        this._fn = fn;
-    }
-    
-    run() {
-        effectActive = this;
-        return this._fn();
-    }
-}
-
-
 // 跟踪依赖
-const targetMap = new Map();
 export const track = (target, key) => {
     // { target: { key: [dep1, dep2, dep3]} }
 
@@ -36,23 +38,31 @@ export const track = (target, key) => {
         targetMap.set(target, depsMap);
     }
 
-    let deps = depsMap.get(key); 
+    let deps = depsMap.get(key);
     // 没有key对应的deps，则生成 key -> deps的依赖
     if (!deps) {
         deps = new Set();
         depsMap.set(key, deps);
     }
-    
+
     deps.add(effectActive);
 }
+
+
+
 
 // 触发依赖
 export const trigger = (traget, key) => {
     const depsMap = targetMap.get(traget);
-    
+
     const deps = depsMap.get(key);
 
     for (let dep of deps) {
-        dep.run();
+        if (dep.scheduler) {
+            dep.scheduler();
+        }
+        else {
+            dep.run();
+        }
     }
 }
--- src/reactive/tests/effect.spec.ts
+++ src/reactive/tests/effect.spec.ts
@@ -34,4 +34,30 @@ describe("effect", () => {
     expect(foo).toBe(12);
     expect(res).toBe('foo');
   })
+
+  it("scheduler", () => {
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj: any = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    // should be called on first trigger
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    // // should not run yet
+    expect(dummy).toBe(1);
+    // // manually run
+    run();
+    // // should have run
+    expect(dummy).toBe(2);
+  });
 });
