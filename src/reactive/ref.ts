18609d0a5e8825a9c1c9894ad3bcd5a29878ce55
linguanghui <1476465526@qq.com>
2022-12-02 18:04:59 +0800

    feat: proxyRefs

--- src/reactive/ref.ts
+++ src/reactive/ref.ts
@@ -57,3 +57,18 @@ export function isRef(ref: any) {
 export function unRef(ref: any) {
   return isRef(ref) ? ref.value : ref;
 }
+
+export function proxyRefs(objectWithRefs: any) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
+}
--- src/reactive/tests/ref.spec.ts
+++ src/reactive/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from '../effect';
 import { reactive } from '../reactive';
-import { ref, isRef, unRef } from '../ref';
+import { ref, isRef, unRef, proxyRefs } from '../ref';
 
 describe('ref', () => {
   it('基本使用', () => {
@@ -31,7 +31,7 @@ describe('ref', () => {
     const obj = ref({ count: 1 });
     let dummy;
     effect(() => {
-        dummy = obj.value.count;
+      dummy = obj.value.count;
     });
     expect(dummy).toBe(1);
     obj.value.count = 2;
@@ -41,7 +41,7 @@ describe('ref', () => {
   it('isRef', () => {
     const a = ref(1);
     const user = reactive({
-        age: 1
+      age: 1,
     });
     expect(isRef(a)).toBe(true);
     expect(isRef(1)).toBe(false);
@@ -51,10 +51,33 @@ describe('ref', () => {
   it('unRef', () => {
     const a = ref(1);
     const user = reactive({
-        age: 1
+      age: 1,
     });
     expect(unRef(a)).toBe(1);
     expect(unRef(1)).toBe(1);
     expect(unRef(user)).toBe(user);
-  })
+  });
+
+  it('proxyRefs', () => {
+    //  该方法常用在template模板解析的时候
+    // setup return {ref}，模板中直接使用，无需使用.value
+    const user = {
+      age: ref(10),
+      name: 'huihui',
+    };
+
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe('huihui');
+
+    proxyUser.age = 20;
+
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+
+    proxyUser.age = ref(10);
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
+  });
 });
