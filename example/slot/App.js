6efcf895208163859d0bd80bca878e82740854fe
linguanghui <1476465526@qq.com>
2022-12-05 22:13:44 +0800

    add: slot案例

---
+++ example/slot/App.js
@@ -0,0 +1,25 @@
+import { h } from '../../lib/mini-vue.esm.js';
+import { Foo } from './Foo.js';
+
+export const App = {
+  name: 'App',
+  render() {
+    const app = h('div', {}, 'ggg');
+    // const foo = h(Foo, {}, [h('p', {}, "123"), h('p', {}, "456")]);
+    const foo = h(
+      Foo,
+      {},
+      {
+        header: ({age}) => h('p', {}, 'header' + age),
+        footer: () => h('p', {}, 'footer'),
+      }
+    );
+    return h('div', {}, [app, foo]);
+  },
+
+  setup() {
+    return {
+      msg: 'mini-vue',
+    };
+  },
+};
