42aab90fc6f95d93a0a6704b3e304d1218449595
linguanghui <1476465526@qq.com>
2022-12-15 21:51:33 +0800

    feat: transform 功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,33 @@
+export function transform(root: any, options: any) {
+  const context = createTransformContext(root, options);
+
+  // 1. 深度优先遍历
+  traverseChildren(root, context);
+  // 2 修改 text content
+}
+
+function traverseChildren(node: any, context: any) {
+  const nodeTransforms = context.nodeTransforms;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node);
+  }
+
+  const children = node.children;
+
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i];
+      traverseChildren(node, context);
+    }
+  }
+}
+
+function createTransformContext(root: any, options: any) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+
+  return context;
+}
